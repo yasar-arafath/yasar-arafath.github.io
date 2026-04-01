5d80dcdf4d6bb544b3fd38010aa847d931795e43
Yasar Arafat <70479774+yasar-arafath@users.noreply.github.com>
2026-04-01 14:27:50 +0900

    static

--- ja/js/custom.js
+++ ja/js/custom.js
@@ -2,6 +2,94 @@
 
   "use strict";
 
+    var LANGUAGE_STORAGE_KEY = 'preferredLanguage';
+
+    function getStoredLanguage() {
+      try {
+        return window.localStorage.getItem(LANGUAGE_STORAGE_KEY);
+      } catch (error) {
+        return null;
+      }
+    }
+
+    function storeLanguage(language) {
+      try {
+        window.localStorage.setItem(LANGUAGE_STORAGE_KEY, language);
+      } catch (error) {
+        // Ignore storage failures so the rest of the page still works.
+      }
+    }
+
+    function isJapanesePath(pathname) {
+      return pathname === '/ja' || pathname.indexOf('/ja/') === 0;
+    }
+
+    function getNormalizedPath(pathname) {
+      if (!pathname || pathname === '/') {
+        return '/index.html';
+      }
+
+      if (pathname === '/ja' || pathname === '/ja/') {
+        return '/index.html';
+      }
+
+      if (pathname.indexOf('/ja/') === 0) {
+        return pathname.replace('/ja/', '/');
+      }
+
+      return pathname;
+    }
+
+    function getLanguageTargetPath(language) {
+      var normalizedPath = getNormalizedPath(window.location.pathname);
+
+      if (language === 'ja') {
+        return '/ja' + normalizedPath;
+      }
+
+      return normalizedPath;
+    }
+
+    function applyStoredLanguage() {
+      var storedLanguage = getStoredLanguage();
+
+      if (storedLanguage !== 'ja' && storedLanguage !== 'en') {
+        return;
+      }
+
+      var currentLanguage = isJapanesePath(window.location.pathname) ? 'ja' : 'en';
+      if (storedLanguage === currentLanguage) {
+        return;
+      }
+
+      var targetPath = getLanguageTargetPath(storedLanguage);
+      var targetUrl = targetPath + window.location.search + window.location.hash;
+      var currentUrl = window.location.pathname + window.location.search + window.location.hash;
+
+      if (targetUrl !== currentUrl) {
+        window.location.replace(targetUrl);
+      }
+    }
+
+    function bindLanguageSelector() {
+      $('.dropdown-item').each(function () {
+        var href = ($(this).attr('href') || '').toLowerCase();
+
+        if (href.indexOf('ja/') !== -1 || href === 'ja' || href === 'ja/index.html') {
+          $(this).attr('data-language', 'ja');
+        } else if (href) {
+          $(this).attr('data-language', 'en');
+        }
+      });
+
+      $('[data-language]').on('click', function () {
+        storeLanguage($(this).attr('data-language'));
+      });
+    }
+
+    applyStoredLanguage();
+    bindLanguageSelector();
+
     // COLOR MODE
     $('.color-mode').click(function(){
         $('.color-mode-icon').toggleClass('active')
